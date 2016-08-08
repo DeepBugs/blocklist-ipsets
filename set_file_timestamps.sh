a4c990f4ca9b0a1172e6815ce5af5bf13b4c086c
Costa Tsaousis <costa@tsaousis.gr>
2016-08-08 11:16:44 +0100

    Mon Aug  8 10:16:43 UTC 2016 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -1023,7 +1023,7 @@
 [ -f 'malwaredomainlist.ipset' ] && /bin/touch --date=@1469127219 'malwaredomainlist.ipset'
 [ -f 'maxmind_proxy_fraud.ipset' ] && /bin/touch --date=@1469442973 'maxmind_proxy_fraud.ipset'
 [ -f 'myip.ipset' ] && /bin/touch --date=@1470612630 'myip.ipset'
-[ -f 'nixspam.ipset' ] && /bin/touch --date=@1470650405 'nixspam.ipset'
+[ -f 'nixspam.ipset' ] && /bin/touch --date=@1470651306 'nixspam.ipset'
 [ -f 'nt_malware_dns.ipset' ] && /bin/touch --date=@1453676702 'nt_malware_dns.ipset'
 [ -f 'nt_malware_http.ipset' ] && /bin/touch --date=@1453676702 'nt_malware_http.ipset'
 [ -f 'nt_malware_irc.ipset' ] && /bin/touch --date=@1453676702 'nt_malware_irc.ipset'
